0ee556595849747df513b2dd3679262be950b100
Mandus Lindström <mandus85@hotmail.com>
2025-01-15 11:46:15 +0100

    Done with wednesday code along, time for video and exercises.

--- react-vite-demo/src/App.tsx
+++ react-vite-demo/src/App.tsx
@@ -1,30 +1,88 @@
+// import "./App.css";
+// import Footer from "./components/Footer/Footer";
+// import Greetings from "./components/Greetings/Greetings";
+// import Header from "./components/Header/Header";
+
+// function App() {
+//   // här är en inline kommentar för en funktion
+//   // function name(params:type) {
+
+//   // }
+
+//   /* här är en multiple rad kommentar för en funktion
+// dsfakhdghasdgl
+// function name(params:type) {
+
+//    }
+// degsaasg
+//   */
+//   const user = { name: "Mandus" };
+
+//   const isLoggedIn = true;
+
+//   // const element = <h3>Hej, {user.name}</h3>;
+
+//   // const element = isLoggedIn ? (
+//   //   <h3>Hej Välkommen, {user.name}</h3>
+//   // ) : (
+//   //   <h3>Logga in!</h3>
+//   // );
+
+//   const items = ["item1", "item2", "item3"];
+
+//   function handleClick(e) {
+//     // alert("Button clicked");
+//     e.preventDefault();
+//     console.log("Button clicked:", e.target.value);
+//   }
+
+//   return (
+//     <>
+//       <Header />
+//       <main>
+//         <Greetings />
+//         <h2>From the teacher</h2>
+//         {/* {element} */}
+
+//         {/* hur man gör det inline: */}
+//         {isLoggedIn ? <h3>Hej Välkommen, {user.name}</h3> : <h3>Logga in!</h3>}
+
+//         <ul>
+//           {items.map((item, index) => (
+//             <li key={index}>{item}</li>
+//           ))}
+//         </ul>
+
+//         <button value={items} onClick={handleClick}>
+//           {/* hur man gör det inline: */}
+//           {/* <button value={items} onClick={() => console.log("object")}> */}
+//           Click me!
+//         </button>
+//       </main>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Greetings from "./components/Greetings/Greetings";
 import Header from "./components/Header/Header";
 
 function App() {
-  // här är en inline kommentar för en funktion
-  // function name(params:type) {
-
-  // }
-
-  /* här är en multiple rad kommentar för en funktion
-dsfakhdghasdgl
-function name(params:type) {
-    
-   }
-degsaasg
-  */
+  const user = {
+    name: "Mandus",
+    lastName: "Lindström",
+  };
+  const h2 = <h2>From the teacher</h2>;
 
   return (
     <>
       <Header />
       <main>
-        <Greetings />
-        <Greetings />
-        <Greetings />
-        <h2>From the teacher</h2>
+        <Greetings user={user} h2={h2} />
       </main>
       <Footer />
     </>
--- react-vite-demo/src/components/Greetings/Greetings.tsx
+++ react-vite-demo/src/components/Greetings/Greetings.tsx
@@ -1,3 +1,20 @@
-export default function Greetings() {
-  return <h1>Hello World</h1>;
+// export default function Greetings(props) {
+export default function Greetings({ user, h2 }) {
+  // console.log("props: ", props.h2);
+  console.log("props: ", h2);
+
+  // const { name, lastName } = props.user;
+  const { name, lastName } = user;
+
+  return (
+    <>
+      <h1>
+        {/* Hello World from {props.name} {props.lastName} */}
+        Hello World from {name} {lastName}
+      </h1>
+      {/* {props.h2} */}
+      {h2}
+    </>
+  );
+  // return <h1>Hello World from </h1>;
 }
